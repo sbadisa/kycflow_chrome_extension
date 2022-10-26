97c7c89539d83bda1c9347ed60441f52c454a30b
Sam Aderounmu <s.b.aderounmu@gmail.com>
2022-10-26 15:56:13 +0100

    initial commit

--- background.js
+++ background.js
@@ -1,12 +1,59 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
+// // check if there is a cookie with the name "sessionid"
+// chrome.cookies.get(
+//   { url: 'http://localhost:8000', name: 'sessionid' },
+//   function (cookie) {}
+// );
+
+//on installation of chrome extension: create contextMenuItem
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'SearchPerson',
+    title: 'Search person "%s" in KYCflow',
+    contexts: ['selection'],
   });
-  
+  chrome.contextMenus.create({
+    id: 'SearchCompany',
+    title: 'Search company "%s" in KYCflow',
+    contexts: ['selection'],
+  });
+  chrome.contextMenus.create({
+    id: 'SearchDefault',
+    title: 'Default search "%s" in KYCflow',
+    contexts: ['selection'],
+  });
+});
+
+//add listener for click on self defined menu item
+chrome.contextMenus.onClicked.addListener(function (clickData, tab) {
+  if (clickData.menuItemId == 'SearchPerson' && clickData.selectionText) {
+    //open new tab
+    chrome.tabs.create({
+      url:
+        'http://localhost:8000/search/?dataset=default&schema=person&q=' +
+        clickData.selectionText +
+        '&url=' +
+        tab.url,
+      index: tab.index + 1,
+    });
+  }
+  if (clickData.menuItemId == 'SearchCompany' && clickData.selectionText) {
+    chrome.tabs.create({
+      url:
+        'http://localhost:8000/search/?dataset=default&schema=company&q=' +
+        clickData.selectionText +
+        '&url=' +
+        tab.url,
+      index: tab.index + 1,
+    });
+  }
+  if (clickData.menuItemId == 'SearchDefault' && clickData.selectionText) {
+    chrome.tabs.create({
+      url:
+        'http://localhost:8000/search/?dataset=default&q=' +
+        clickData.selectionText +
+        '&url=' +
+        tab.url,
+      index: tab.index + 1,
+    });
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,55 @@
-(() => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
-    let currentVideoBookmarks = [];
+// (() => {
+//     let youtubeLeftControls, youtubePlayer;
+//     let currentVideo = "";
+//     let currentVideoBookmarks = [];
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
+//     chrome.runtime.onMessage.addListener((obj, sender, response) => {
+//         const { type, value, videoId } = obj;
 
-        if (type === "NEW") {
-            currentVideo = videoId;
-            newVideoLoaded();
-        }
-    });
+//         if (type === "NEW") {
+//             currentVideo = videoId;
+//             newVideoLoaded();
+//         }
+//     });
 
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
+//     const newVideoLoaded = () => {
+//         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+//         console.log(bookmarkBtnExists);
 
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+//         if (!bookmarkBtnExists) {
+//             const bookmarkBtn = document.createElement("img");
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
+//             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+//             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+//             bookmarkBtn.title = "Click to bookmark current timestamp";
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+//             youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+//             youtubePlayer = document.getElementsByClassName("video-stream")[0];
             
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-        }
-    }
+//             youtubeLeftControls.append(bookmarkBtn);
+//             bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+//         }
+//     }
 
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
+//     const addNewBookmarkEventHandler = () => {
+//         const currentTime = youtubePlayer.currentTime;
+//         const newBookmark = {
+//             time: currentTime,
+//             desc: "Bookmark at " + getTime(currentTime),
+//         };
+//         console.log(newBookmark);
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
+//         chrome.storage.sync.set({
+//             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+//         });
+//     }
 
-    newVideoLoaded();
-})();
+//     newVideoLoaded();
+// })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
+// const getTime = t => {
+//     var date = new Date(0);
+//     date.setSeconds(1);
 
-    return date.toISOString().substr(11, 0);
-}
+//     return date.toISOString().substr(11, 0);
+// }
